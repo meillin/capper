92a0d68fda56eb2aa7b8cb0ad45890a77edac29f
Meilan <meilan.dk@gmail.com>
2014-02-17 22:47:04 +0100

    Prototype controller inside another controller

--- js/controller/index_ctrl.js
+++ js/controller/index_ctrl.js
@@ -2,4 +2,8 @@ define(['app'], function (app) {
     app.register.controller('IndexController', function ($scope) {
 		$scope.pagetitle = "INDEX";
     });
+
+    app.register.controller('testController', function($scope) {
+    	$scope.pagetitle = "testController"
+    })
 });
