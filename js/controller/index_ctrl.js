c0a79ad779e61f3820b7b5924ace9e1e7eb59817
Meilan Lin <meilan.lin@capgemini.com>
2014-02-18 11:21:27 +0100

    Made some useless controllers

--- js/controller/index_ctrl.js
+++ js/controller/index_ctrl.js
@@ -1,9 +1,32 @@
 define(['app'], function (app) {
+
+	var imgPath = '../../img/';
+
     app.register.controller('IndexController', function ($scope) {
 		$scope.pagetitle = "INDEX";
     });
 
-    app.register.controller('testController', function($scope) {
-    	$scope.pagetitle = "testController"
-    })
+    app.register.controller('BowerController', function($scope) {
+		$scope.logoUrl = imgPath + 'bower.png';
+	});
+
+    app.register.controller('SassController', ['$scope', function($scope){
+		$scope.logoUrl = imgPath + 'sass.png';
+    }]);
+
+	app.register.controller('AngularController', function($scope){
+		$scope.logoUrl = imgPath + 'angularjs.jpg';
+	});
+
+	app.register.controller('GruntController', ['$scope', function($scope){
+		$scope.logoUrl = imgPath + 'grunt.png';
+	}]);
+
+	app.register.controller('BackboneController', ['$scope', function($scope){
+		$scope.logoUrl = imgPath + 'backbone.png';
+	}]);
+
+	app.register.controller('SublimeController', ['$scope', function($scope){
+		$scope.logoUrl = imgPath + 'Sublimetext.png';
+	}]);
 });
